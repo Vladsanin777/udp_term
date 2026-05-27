94d5f8a4d77a558933a9453a9daf6880356dda0e
Vladsanin777 <vlad.sanin.2007.09@gmail.com>
2026-05-27 07:43:26 +0400

    intermediate

--- ui/Main.cpp
+++ ui/Main.cpp
@@ -1,40 +1,48 @@
+#include <QApplication>
+#include <QWidget>
 #include <QWindow>
 #include <QPushButton>
-#include <QTextLine>
+#include <QLineEdit>
 #include <QTextEdit>
+#include <QVBoxLayout>
 
-class UDP_Window : public QWindow {
+class UDP_Window : public QWidget {
 private:
-    QVBoxLayout m_layout{this};
-    QTextLine m_ipSource{&m_layout};
-    QTextLine m_ipDestantion{&m_layout};
-    QTextLine m_portSource{&m_layout};
-    QTextLine m_portDestantion{&m_layout};
-    QTextLine m_macSource{&m_layout};
-    QTextLine m_macDestantion{&m_layout};
-    QTextLine m_interface{&m_layout};
-    QTextEdit m_data{&m_layout};
-    QTextLine m_fileName{&m_layout};
-    QPushButton m_send{&m_layout};
+    QVBoxLayout m_layout;
+    QLineEdit m_ipSource;
+    QLineEdit m_ipDestantion;
+    QLineEdit m_portSource;
+    QLineEdit m_portDestantion;
+    QLineEdit m_macSource;
+    QLineEdit m_macDestantion;
+    QLineEdit m_interface;
+    QTextEdit m_data;
+    QLineEdit m_fileName;
+    QPushButton m_send;
+    bool m_initialized = false;
 
 public:
-    UDP_Window(QWindow * parent = nullptr) : QWindow{parent} {
-        m_layout.addWidget(m_ipSource);
-        m_layout.addWidget(m_ipDestantion);
-        m_layout.addWidget(m_portSource);
-        m_layout.addWidget(m_portDestantion);
-        m_layout.addWidget(m_macSource);
-        m_layout.addWidget(m_macDestantion);
-        m_layout.addWidget(m_interface);
-        m_layout.addWidget(m_data);
-        m_layout.addWidget(m_fileName);
-        m_layout.addWidget(m_send);
+    UDP_Window(QWidget * parent = nullptr) : QWidget{parent},
+            m_layout{this}, m_send{"Send"} {
+        setLayout(&m_layout);
 
-        setLayout(m_layout);
+        m_layout.addWidget(&m_ipSource);
+        m_layout.addWidget(&m_ipDestantion);
+        m_layout.addWidget(&m_portSource);
+        m_layout.addWidget(&m_portDestantion);
+        m_layout.addWidget(&m_macSource);
+        m_layout.addWidget(&m_macDestantion);
+        m_layout.addWidget(&m_interface);
+        m_layout.addWidget(&m_data);
+        m_layout.addWidget(&m_fileName);
+        m_layout.addWidget(&m_send);
     }
 };
 
-int main() {
+int main(int argc, char ** argv) {
+    QApplication app{argc, argv};
     UDP_Window window{};
-    return 0;
+    window.resize(350, 350);
+    window.show();
+    return app.exec();
 }
